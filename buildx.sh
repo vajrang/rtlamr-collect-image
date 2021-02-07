c356e54ab0558b25111f1732928a0ed5d42a9218
Vajrang Parvate <vparvate@simscale.com>
2021-02-06 20:26:43 -0500

    another fix

--- buildx.sh
+++ buildx.sh
@@ -2,8 +2,10 @@
 
 # from https://www.docker.com/blog/multi-arch-build-and-images-the-simple-way/
 
+echo ==== Starting buildx.sh ====
 NEWBUILDX=$(docker buildx create --use)
 docker buildx build --push --platform linux/arm64,linux/amd64,linux/arm/v7 --tag vajrang/rtlamr-collect:latest .
 docker buildx use default
 docker buildx rm $NEWBUILDX
 docker buildx prune -f
+echo ==== Finished buildx.sh ====
